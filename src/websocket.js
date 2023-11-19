490c3c0d632e568b91d8a0f83687b58b28092265
JulienF777 <juju56260@live.fr>
2023-11-19 17:16:12 -0500

    test1

--- src/websocket.js
+++ src/websocket.js
@@ -7,12 +7,48 @@ let ws = new WebSocket("ws://kevin-chapron.fr:8090/ws");
             var jsonapp = JSON.stringify(app);
             ws.send(jsonapp);
 
-            //test du control 1 avec bouton
-            document.getElementById("test").addEventListener("click", function (event) {
+            //Bouton haut, envoi
+            document.getElementById("haut").addEventListener("click", function (event) {
      
-              let testcontrol = "test du contrôle 1";
+              let hautcontrol = "haut";
               var sendControl = {
-                message: testcontrol
+                message: hautcontrol
+              };
+              var jsonControl = JSON.stringify(sendControl);
+              ws.send(jsonControl);
+              
+              });
+
+              //Bouton gauche, envoi
+            document.getElementById("gauche").addEventListener("click", function (event) {
+     
+              let gauchecontrol = "gauche";
+              var sendControl = {
+                message: gauchecontrol
+              };
+              var jsonControl = JSON.stringify(sendControl);
+              ws.send(jsonControl);
+              
+              });
+
+              //Bouton droite, envoi
+            document.getElementById("droite").addEventListener("click", function (event) {
+     
+              let droitecontrol = "droite";
+              var sendControl = {
+                message: droitecontrol
+              };
+              var jsonControl = JSON.stringify(sendControl);
+              ws.send(jsonControl);
+              
+              });
+
+              //Bouton bas, envoi
+            document.getElementById("bas").addEventListener("click", function (event) {
+     
+              let bascontrol = "bas";
+              var sendControl = {
+                message: bascontrol
               };
               var jsonControl = JSON.stringify(sendControl);
               ws.send(jsonControl);
@@ -23,7 +59,20 @@ let ws = new WebSocket("ws://kevin-chapron.fr:8090/ws");
             console.log("Disconnected from websocket !");
           };
           ws.onmessage = function (event) {
-            console.log("Message received from websocket : " + event.data);
+            //on récupère le message (controle) reçu
+            console.log("websocket:"+event.data);
+            var parseControl = JSON.parse(event.data);  
+            //on vérifie si le message correspond à un controle
+            if (parseControl.message == "haut"){
+              console.log("le contrôle est haut")
+            } else if (parseControl.message == "gauche"){
+              console.log("le contrôle est gauche")
+            } else if (parseControl.message == "droite"){
+              console.log("le contrôle est droite")
+            } else if (parseControl.message == "bas"){
+              console.log("le contrôle est bas")
+            }
+
           };
           ws.onerror = function (event) {
             console.log("Error Websocket : " + event.message);
