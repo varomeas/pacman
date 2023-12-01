61f01c1025fc629721d4abc9594104f068b19c56
JulienF777 <juju56260@live.fr>
2023-12-01 15:30:40 -0500

    map update multijoueur sur le websocket

--- src/websocket.js
+++ src/websocket.js
@@ -1,5 +1,101 @@
 
-import { movePlayer } from "./grid";
+const canvas = document.getElementById("grid");
+const context = canvas.getContext("2d");
+
+//Paramètres de la grid
+const tileWidth = 40;
+const tileHeight = 40; 
+const gridRows = 10;
+const gridCols = 10;
+
+//défiinir chaque variable
+//let boost = 1>>2
+
+//map de 10 par 10
+let map = [
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,1,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+  0,0,0,0,1,0,0,0,0,0,
+]
+
+const updateAll = () => {
+  drawMap();
+  window.requestAnimationFrame(updateAll);
+}
+
+window.onload = () => {
+  window.requestAnimationFrame(updateAll);
+}
+
+function drawMap(newmap){
+  for(let eachRow = 0; eachRow < gridRows; eachRow++){
+    for(let eachCol = 0; eachCol < gridCols; eachCol++){
+      //on parcours chaque élément du tableau map
+      let arrayIndex = eachRow * gridRows + eachCol;
+      //on vérifie si l'élément est un 0(chemin), un 1(mur) ou autre(joueur)
+      if(newmap[arrayIndex] === 1){
+        //pour espacer les blocks, on fait tileWidth*eachCol pour créer un espace dans chaque block sur l'axe horizontal, puis pareil pour le vertical avec tileHeight*eachRow.
+        //sans ce code, toutes les cases se retrouvent les une par dessu les autres.
+        context.fillStyle="lightgrey";
+        context.fillRect(tileWidth*eachCol,tileHeight*eachRow,tileWidth,tileHeight);
+      } else if(newmap[arrayIndex] === 0) {
+        context.fillStyle="black";
+        context.fillRect(tileWidth*eachCol,tileHeight*eachRow,tileWidth,tileHeight);
+      } else {
+        context.fillStyle="blue";
+        context.fillRect(tileWidth*eachCol,tileHeight*eachRow,tileWidth,tileHeight);
+      }
+    }
+  }
+}
+
+//avoir le joueur en numéro 2 dans le tableau map pourrait créer des pb
+//je pense qu'il faudrait avoir 2 couches: une constante map avec des 0 ou 1
+//une autre couche "joueurs" qui vérifie les murs et chemins.
+
+//player
+const spawnPlayer = () => {
+  //position de départ
+  let spawnPosition = 5;
+  //update le nombre de l'array map à 2 pour représenter le joueur:
+  //2 représente le joueur 1.
+  map[spawnPosition] = 2;
+}
+
+const getPlayerPosition = () => {
+  let playerIndex = map.indexOf(2);
+  //le player position est un index (0=première case de map)
+  //console.log("playerposition"+playerIndex);
+  return playerIndex;
+}
+
+const movePlayer = (move) => {
+  let playerPosition = getPlayerPosition();
+  //on reset l'emplacement présent du joueur à 0 pour remettre un vide
+  //avant de bouger le joueur
+  map[playerPosition] = 0;
+  //on bouge le joueur
+  let newposition = playerPosition+move;
+  map[newposition] = 2;
+  //2 représente le joueur sur la map.
+}
+//export de moveplayer pour faire marcher les boutons de controles dans websocket.js
+export {movePlayer};
+export {map};
+export {updateAll};
+export {drawMap};
+//on spawn le joueur au début de la partie.
+spawnPlayer();
+
+//todo:
+//créer un constructeur pour les murs, vérifier la collision
 
 let ws = new WebSocket("ws://kevin-chapron.fr:8090/ws");
         ws.onopen = function (event) {
@@ -10,53 +106,46 @@ let ws = new WebSocket("ws://kevin-chapron.fr:8090/ws");
             var jsonapp = JSON.stringify(app);
             ws.send(jsonapp);
 
-            //Bouton haut, envoi
-            document.getElementById("haut").addEventListener("click", function (event) {
-     
-              let hautcontrol = "haut";
-              var sendControl = {
-                message: hautcontrol
-              };
-              var jsonControl = JSON.stringify(sendControl);
-              ws.send(jsonControl);
-              
-              });
+            //Boutons touches clavier
+            document.addEventListener('keydown', function(event) {
+              switch(event.key) {
+                case 'ArrowUp':
+                  let hautcontrol = "haut";
+                  var sendControl = {
+                    message: hautcontrol
+                  };
+                  var jsonControl = JSON.stringify(sendControl);
+                  ws.send(jsonControl);
+                  break;
 
-              //Bouton gauche, envoi
-            document.getElementById("gauche").addEventListener("click", function (event) {
-     
-              let gauchecontrol = "gauche";
-              var sendControl = {
-                message: gauchecontrol
-              };
-              var jsonControl = JSON.stringify(sendControl);
-              ws.send(jsonControl);
-              
-              });
+                case 'ArrowDown':
+                  let bascontrol = "bas";
+                  var sendControl = {
+                    message: bascontrol
+                  };
+                  var jsonControl = JSON.stringify(sendControl);
+                  ws.send(jsonControl);
+                  break;
 
-              //Bouton droite, envoi
-            document.getElementById("droite").addEventListener("click", function (event) {
-     
-              let droitecontrol = "droite";
-              var sendControl = {
-                message: droitecontrol
-              };
-              var jsonControl = JSON.stringify(sendControl);
-              ws.send(jsonControl);
-              
-              });
+                case 'ArrowLeft':
+                  let gauchecontrol = "gauche";
+                  var sendControl = {
+                    message: gauchecontrol
+                  };
+                  var jsonControl = JSON.stringify(sendControl);
+                  ws.send(jsonControl);
+                  break;
 
-              //Bouton bas, envoi
-            document.getElementById("bas").addEventListener("click", function (event) {
-     
-              let bascontrol = "bas";
-              var sendControl = {
-                message: bascontrol
-              };
-              var jsonControl = JSON.stringify(sendControl);
-              ws.send(jsonControl);
-              
-              });
+                case 'ArrowRight':
+                  let droitecontrol = "droite";
+                  var sendControl = {
+                    message: droitecontrol
+                  };
+                  var jsonControl = JSON.stringify(sendControl);
+                  ws.send(jsonControl);
+                  break;
+              }
+            });
           };
           ws.onclose = function (event) {
             console.log("Disconnected from websocket !");
@@ -64,7 +153,9 @@ let ws = new WebSocket("ws://kevin-chapron.fr:8090/ws");
           ws.onmessage = function (event) {
             //on récupère le message (controle) reçu
             console.log("websocket:"+event.data);
-            var parseControl = JSON.parse(event.data);  
+            var newmap = JSON.parse(event.data);  
+            var parseControl = JSON.parse(event.data); 
+
             //on vérifie si le message correspond à un controle
             if (parseControl.message == "haut"){              
               movePlayer(-10);
@@ -76,8 +167,23 @@ let ws = new WebSocket("ws://kevin-chapron.fr:8090/ws");
               movePlayer(10);
             }
 
+            // newmap = map + move du joueur
+
+            drawMap(newmap.message);
+
           };
           ws.onerror = function (event) {
             console.log("Error Websocket : " + event.message);
           };
 
+          function myFunction() {
+            // Your code logic here
+            var jsonmap = {
+              message: map
+            };
+            var sendmap = JSON.stringify(jsonmap);
+            ws.send(sendmap);
+          }
+          
+          // Set the interval to run the function every 100 milliseconds
+          setInterval(myFunction, 2000);
